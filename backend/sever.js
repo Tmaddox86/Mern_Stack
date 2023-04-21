a80b0c99df468e146ed8739ccf8c6ce9f6c4da3b
Tara Maddox <TMaddox85@gmail.com>
2023-04-21 00:08:34 -0700

    added Schema

--- backend/sever.js
+++ backend/sever.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const patientRoutes = require('./routes/patientsRoute')
+
+
 //express app
 const app = express()
 
@@ -16,6 +19,17 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/patients', patientRoutes)
 
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on PORT', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 /*app.get('/', (req,res) => {
     res.json({message: 'Welcome To the App'})
 })*/
@@ -23,8 +37,8 @@ app.use('/api/patients', patientRoutes)
 
 
 //listen for request
-app.listen(process.env.PORT, () => {
+/*app.listen(process.env.PORT, () => {
     console.log('listening on PORT 4000')
-})
+})*/
 
 process.env
